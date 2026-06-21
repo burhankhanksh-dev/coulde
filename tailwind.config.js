fadd662adfe4d72f73297927ffb4edda47103ecd
burhankhanksh-dev <burhankhanksh@gmail.com>
2026-06-22 02:32:02 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          DEFAULT: '#D4AF37',
+          dark: '#C5A028',
+          light: '#F1D18A',
+        },
+      },
+      fontFamily: {
+        serif: ['Playfair Display', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
